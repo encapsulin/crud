e887ced974ec42178c99bdb10c6eccd71959c07e
encapsulin <encapsulin@gmail.com>
2024-10-03 12:06:16 +0200

    post 100 items to dynamo

--- lambda/tests.sh/rest-post.sh
+++ lambda/tests.sh/rest-post.sh
@@ -1,5 +1,14 @@
-source ./sh/cfg.sh
+pwd
+source ./lambda/tests.sh/cfg.sh
  
+for i in {1..100}; do
+    strDT=$(date +'%Y%m%d-%H%M%S')
+    strDT="${strDT}.${i}"
+    echo $strDT
+
 curl -X POST $URL \
 -H "Content-Type: application/json" \
--d '{"parent":"0","role":"doc","descr":"qwer ","pkid":"0","title":"doc011","skid":"20240918_202319_194"}'
+-H "Authorization: 46068b06-db78-46dc-b929-9109185278b4" \
+-d "{\"parent\":\"20240929_160937_354\",\"role\":\"doc\",\"descr\":\"$strDT\",\"title\":\"$strDT\",\"skid\":\"0\"}"
+
+done
---
+++ lambda/tests.sh/test.sh
@@ -0,0 +1,2 @@
+strDT=`date +"%Y%m%d-%H%M%S"`
+echo $strDT

4d8ee84a18003a49c02834afd955d2d410eb4f79
encapsulin <encapsulin@gmail.com>
2024-12-27 10:48:19 +0100

    align containers portrait/landscape

--- Front/angular/crud/src/app/crud/items/items.component.ts
+++ Front/angular/crud/src/app/crud/items/items.component.ts
@@ -9,11 +9,12 @@ import { Component } from '@angular/core';
 })
 export class ItemsComponent {
   items = [
-    {"id":1,"title":"Вареники","descr":" з сиром солоні ","price":4, "img":"img/2.jpeg"},
-    {"id":2,"title":"Вареники","descr":" з сиром солодкі","price":6, "img":"img/2.jpeg"},
-    {"id":3,"title":"Вареники ","descr":"(картопля)","price":5, "img":"img/3.jpeg"},
-    {"id":1,"title":"Вареники ","descr":"(картопля, печінка)","price":4, "img":"img/4.jpeg"},
-    {"id":2,"title":"Пельмені ","descr":"(свин)","price":6, "img":"img/5.jpeg"},
-    {"id":2,"title":"Пельмені","descr":" (кур)","price":6, "img":"img/6.jpeg"}
+    {"id":1,"title":"Вареники","descr":" з сиром солоні ","price":"1/100", "img":"img/2.jpeg"},
+    {"id":2,"title":"Вареники","descr":" з сиром солодкі","price":"1/100", "img":"img/2.jpeg"},
+    {"id":3,"title":"Вареники","descr":"(картопля)","price":"1/100", "img":"img/3.jpeg"},
+    {"id":1,"title":"Вареники","descr":"(картопля, печінка)","price":"1/100", "img":"img/4.jpeg"},
+    {"id":2,"title":"Пельмені","descr":"(свин)","price":"1/100", "img":"img/5.jpeg"},
+    {"id":2,"title":"Пельмені","descr":" (кур)","price":"1/100", "img":"img/6.jpeg"},
+    {"id":2,"title":"Холодець","descr":" (свин + кур)","price":"4/300", "img":"img/1.jpeg"}
   ]
 }
